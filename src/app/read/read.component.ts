98bacf1c9902d4947e4235f40510146806bd6ae8
feelipefreitas <feelipe.freitas97@gmail.com>
2019-03-01 00:49:05 -0300

    NGRX

---
+++ src/app/read/read.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Store } from '@ngrx/store';
+import { Tutorial } from './../models/tutorial.model';
+import { AppState } from './../app.state';
+import { Observable } from 'rxjs';
+import * as TutorialActions from './../actions/tutorial.actions';
+
+@Component({
+  selector: 'app-read',
+  templateUrl: './read.component.html',
+  styleUrls: ['./read.component.css']
+})
+export class ReadComponent implements OnInit {
+
+  tutorials: Observable<Tutorial[]>;
+
+  constructor(private store: Store<AppState>) {
+    this.tutorials = this.store.select('tutorial');
+  }
+
+  removeTutorial(i) {
+    this.store.dispatch(new TutorialActions.RemoveTutorial(i));
+  }
+
+  ngOnInit() {
+  }
+
+}
